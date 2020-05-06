4e2072df465ca12626f58cbc872f8f788c4d8ded
amunozpe@vectoritcgroup.com <amunozpe@vectoritcgroup.com>
2020-05-06 19:14:32 +0200

    up

--- src/main/java/com/in28minutes/microservices/serviciousuarios2/configuration/SecurityConfigAgu.java
+++ src/main/java/com/in28minutes/microservices/serviciousuarios2/configuration/SecurityConfigAgu.java
@@ -1,8 +1,38 @@
 package com.in28minutes.microservices.serviciousuarios2.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfigAgu extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+  /* http
+      .cors()
+      .and()
+      .csrf().disable()
+     // .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+     // .and()
+      .authorizeRequests()
+      .antMatchers("/**").permitAll();*/
+
+
+    http
+      .csrf().disable()
+      .authorizeRequests()
+      .antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+      .anyRequest().authenticated()
+      .and()
+      .httpBasic();
+
+
+
+  }
+
+
+
+
 }
