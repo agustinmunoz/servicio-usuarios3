44fe2227594c08ed40cd08908a4efa983b50a9c0
amunozpe@vectoritcgroup.com <amunozpe@vectoritcgroup.com>
2020-05-06 16:40:07 +0200

    up

---
+++ src/test/java/com/in28minutes/microservices/serviciousuarios2/ServicioUsuarios2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.serviciousuarios2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioUsuarios2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
