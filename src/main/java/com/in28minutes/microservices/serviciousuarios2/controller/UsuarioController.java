a5b6bfa1f9ec08be3be84c80039841f7bd5ba6b2
amunozpe@vectoritcgroup.com <amunozpe@vectoritcgroup.com>
2020-05-06 12:41:51 +0200

    update

--- src/main/java/com/in28minutes/microservices/serviciousuarios2/controller/UsuarioController.java
+++ src/main/java/com/in28minutes/microservices/serviciousuarios2/controller/UsuarioController.java
@@ -5,11 +5,13 @@ import com.in28minutes.microservices.serviciousuarios2.repository.UsuarioReposit
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 public class UsuarioController {
 
     @Autowired
