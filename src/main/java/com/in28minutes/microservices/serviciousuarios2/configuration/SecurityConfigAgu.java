e11452cc00d625062a93c93d7bc5a4488a71482d
amunozpe@vectoritcgroup.com <amunozpe@vectoritcgroup.com>
2020-05-06 19:11:24 +0200

    up

---
+++ src/main/java/com/in28minutes/microservices/serviciousuarios2/configuration/SecurityConfigAgu.java
@@ -0,0 +1,8 @@
+package com.in28minutes.microservices.serviciousuarios2.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfigAgu extends WebSecurityConfigurerAdapter {
+}
